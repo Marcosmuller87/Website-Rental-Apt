b37d6db146b17abede01e08f33bb0dc9f7daa91c
Marcosmuller87 <marcosmuller87@gmail.com>
2021-07-23 12:01:52 -0300

    Nuevos Cambios

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,10 +5,10 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <video src="/videos/beach.mp4" autoPlay loop muted />
-      <h1>APARTAMENTO DA SOLI</h1>
+      <h1>APARTAMENTO DA DONA SOLI</h1>
       <h2>
         O que vocês achariam de se hospedarem na Praia do Morro a só duas
-        quadras do mar? Estamos aguardando voces!!!
+        quadras do mar? Eu so a Dona Solidade e estou aguardando voces!!!
       </h2>
     </div>
   );
